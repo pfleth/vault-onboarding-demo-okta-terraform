6fe151524ab97beee41a7d4ff7335513d85a4cd7
Drew Mullen <drew.mullen@hashicorp.com>
2026-01-14 08:58:38 -0500

    update outputs

--- 2_onboard_app/outputs.tf
+++ 2_onboard_app/outputs.tf
@@ -1,9 +1,7 @@
 output "groups" {
   value = {
     okta_group = {
-      for k, v in okta_group.enumeration : k => {
-        id           = v.id
-        name         = v.name
+      for k, v in okta_group.enumeration : v.name => {
         description  = v.description
         vault_policy = vault_policy.enumeration[k].policy
       }
