0152347502fc15e109235e0540388b47d28658e0
Drew Mullen <drew.mullen@hashicorp.com>
2026-01-14 09:00:31 -0500

    update outputs

--- 2_onboard_app/outputs.tf
+++ 2_onboard_app/outputs.tf
@@ -1,6 +1,6 @@
 output "groups" {
   value = {
-    okta_group = {
+    "Request access to any of these okta_groups through sailpoint" = {
       for k, v in okta_group.enumeration : v.name => {
         description  = v.description
         vault_policy = vault_policy.enumeration[k].policy
